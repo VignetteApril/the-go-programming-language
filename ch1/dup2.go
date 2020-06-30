10b066755da38a96453e7ddbb40a25d7400f7a92
zhangjie <zhangjieboot@gmail.com>
2020-06-30 10:46:29 +0800

    chi update

---
+++ ch1/dup2.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"image"
+	"image/color"
+	"image/gif"
+	"io"
+	"math"
+	"math/rand"
+	"os"
+)
+
+var palette = []color.Color{color.White, color.Black}
+
+const {
+	whiteIndex = 0
+	blackIndex = 1
+}
+
+func main() {
+	lissajous(os.Stdout)
+}
+
+func lissajous(out io.Writer) {
+	const {
+		cycles 	= 5
+		res    	= 0.001
+		size   	= 100
+		nframes = 64
+		delay  	= 8
+	}
+	freq := rand.Float64() * 3.0
+}
